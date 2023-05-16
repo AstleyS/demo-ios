ee2e65299024248b2bddd9158e6c60a755a07a97
Astley Santos <a21704176@alunos.ulht.pt>
2023-05-16 20:00:05 +0200

    Initial Commit

---
+++ Demo/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Demo
+//
+//  Created by Astley Santos on 15/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+
+
---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Astley Santos on 15/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
